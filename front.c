09a88d761bb4ae6600fb0da6f65f81f89d05a77a
Yure Domingues <yure.domingues@gmail.com>
2023-11-23 23:55:28 -0300

    Adicionando front

---
+++ front.c
@@ -0,0 +1,66 @@
+#include "headers/front.h"
+
+const SDL_Color background = {.r = 38, .g = 35, .b = 34, .a = SDL_ALPHA_OPAQUE}; //Hexadecimal color: #7A96B8
+const SDL_Color bar = {.r = 153, .g = 194, .b = 77, .a = SDL_ALPHA_OPAQUE}; //Hexadecimal color: #99C24D
+const SDL_Color grid = {.r = 102, .g = 133, .b = 134, .a = SDL_ALPHA_OPAQUE}; //Hexadecimal color: #2F4B26
+const SDL_Color line = {.r = 252, .g = 247, .b = 248, .a = SDL_ALPHA_OPAQUE}; //Hexadecimal color: ##FCF7F8
+
+void get_text_and_rect(SDL_Renderer *renderer, int x, int y, char *text, TTF_Font *font, SDL_Texture **texture, SDL_Rect *rect) {
+    int text_width;
+    int text_height;
+    SDL_Surface *surface;
+    SDL_Color textColor = {117, 221, 221, 0};
+
+    surface = TTF_RenderText_Solid(font, text, textColor);
+    *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    text_width = surface->w;
+    text_height = surface->h;
+    SDL_FreeSurface(surface);
+    rect->x = x;
+    rect->y = y;
+    rect->w = text_width;
+    rect->h = text_height;
+}
+
+void draw_headers(char* header1, char* header2, SDL_Renderer *renderer, TTF_Font *font, SDL_Texture **texture, SDL_Rect *rect){
+    get_text_and_rect(renderer, 10,0, header1, font,  texture, rect);
+    SDL_RenderCopy(renderer, *texture, NULL, rect);
+    SDL_DestroyTexture(*texture);
+
+    get_text_and_rect(renderer, 10, WINDOWS_HIGH/2+3, header2, font,  texture, rect);
+    SDL_RenderCopy(renderer, *texture, NULL, rect);
+    SDL_DestroyTexture(*texture);
+}
+
+void draw_bar(SDL_Renderer* renderer, int x, int y, int h, int w){
+    SDL_SetRenderDrawColor(renderer, bar.r, bar.g, bar.b, bar.a); 
+    SDL_Rect rect = {.x = x, .y = y, .h = h, .w = w};
+    SDL_RenderFillRect(renderer, &rect);
+    SDL_SetRenderDrawColor(renderer, line.r, line.g, grid.b, line.a); 
+    SDL_RenderDrawRect(renderer, &rect);
+}
+
+void draw_bar_ref(SDL_Renderer* renderer, int pos, int high){
+    draw_bar(renderer, 80+pos*BAR_WIDTH, WINDOWS_HIGH/2-high, high, BAR_WIDTH);
+}
+
+void draw_bar_mark(SDL_Renderer* renderer, int pos, int high){
+    draw_bar(renderer, 80+pos*BAR_WIDTH, WINDOWS_HIGH-high, high, BAR_WIDTH);
+}
+
+void draw_lines(SDL_Renderer* renderer){
+    int xi = 80;
+    int xf = WINDOWS_WIDTH;
+    int h = MAX_BAR_HIGH;
+    int div_part = 4;
+
+    SDL_SetRenderDrawColor(renderer, line.r, line.g, line.b, line.a);
+    for(int i=0; i<=4; i++){
+        SDL_RenderDrawLine(renderer, xi, WINDOWS_HIGH/2 - (h/div_part)*i, xf, WINDOWS_HIGH/2 - (h/div_part)*i);
+    }
+    for(int i=0; i<=4; i++){
+        SDL_RenderDrawLine(renderer, xi, WINDOWS_HIGH - (h/div_part)*i, xf, WINDOWS_HIGH - (h/div_part)*i);
+    }
+    
+
+}
--- main.c
+++ main.c
@@ -1,14 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "headers/contagem-referencia.h"
+#include "headers/front.h"
 
-int main(){
-    int *v = (int*) malloc2(sizeof(int));
-    *v = 10;
-    int *w = (int*) malloc2(sizeof(int));
-    *w = 20;
-    atrib2(&v,w);
-    atrib2(&w,NULL);
-    heap_status();
+int main() {
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+    SDL_Texture* header_texture = NULL;
+    SDL_Texture* time_texture;
+    SDL_Rect header_rect;
+    TTF_Font *font = NULL;
+
+    TTF_Init();
+
+    font = TTF_OpenFont("fonts/Go-Bold.ttf", 24);
+    if (font == NULL) {
+        fprintf(stderr, "Erro: fonte não encontrada.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+
+    if ( SDL_Init(SDL_INIT_VIDEO) == 0 ){
+        window = SDL_CreateWindow("Heap usage", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOWS_WIDTH, WINDOWS_HIGH, SDL_WINDOW_SHOWN);
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    }
+    else
+        SDL_Log("Erro na inicialização: %s", SDL_GetError());
+
+    if ( window != NULL ){
+        
+        while( !SDL_QuitRequested() ) {
+            SDL_SetRenderDrawColor(renderer, background.r, background.g, background.b, background.a); 
+            SDL_RenderClear(renderer);
+
+            for(int i = 0; i<120; i++){
+                
+                
+                if(i <= 100){
+                    draw_bar_ref(renderer, i, 2*i);
+                }else{
+                    draw_bar_ref(renderer, i, 200);
+                }
+            }
+        
+            draw_lines(renderer);
+            draw_headers("Contagem por referencia", "Mark-and-Sweep", renderer, font, &header_texture, &header_rect);
+            
+            SDL_RenderPresent(renderer);
+        }
+    }
+    else
+        SDL_Log("Erro na criação da janela: %s", SDL_GetError());
+
+    TTF_CloseFont(font);
+    TTF_Quit();
+
+    SDL_DestroyTexture(header_texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
-};
+}
